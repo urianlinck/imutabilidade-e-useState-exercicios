f7b0c491f2f9eb563e46eae9a3d6ca8cf795b08a
Luiz Fernando Barbosa <71241157+luizfbarbosa12@users.noreply.github.com>
2022-10-17 10:45:47 -0300

    exercicio useState

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import styled, {createGlobalStyle} from 'styled-components'
+import PokemonCard from './Components/PokemonCard/PokemonCard';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+function App() {
+  const [pokemon, setPokemon] = useState({
+    name: "Pichu",
+    type: "Electric",
+    evolved: false,
+    weight: 2,
+    color: 'yellow',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
+    id: 0
+  })
+
+  // Para fazer seus próximos pokemons, crie novos estados!
+
+  return ( <>
+  <GlobalStyles/>
+    <FlexContainer>
+      <PokemonCard pokemon={pokemon} setPokemon={setPokemon}/>
+      {/* Crie aqui seus próximos pokemons! */}
+    </FlexContainer>
+  </>
+    
+  );
+}
+
+export default App;
